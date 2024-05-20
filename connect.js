788c1eac6e15f42f1b475da98ffefd49ed9c0f81
Himanshu Kunwar <himanshukunwar07@gmail.com>
2024-05-20 13:45:02 +0530

    index updated

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 async function connectToMongoDB(url){
-    return mongoose.connect(url);
+    return mongoose.connect(url ?? "mongodb://localhost:27017/shortUrl");
 }
 
 module.exports = {
